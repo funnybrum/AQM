9040af815fa687bd4c9ced21acb1181a4d48537b
funnybrum <funnybrum@gmail.com>
2018-10-07 09:11:12 +0300

    Updating the secrets.h file with proper values.

--- src/Secrets.h
+++ src/Secrets.h
@@ -3,7 +3,7 @@
 
 // Execut 'git update-index --assume-unchanged ./src/Secrets.h' to avoid tracking changes to this file.'
 // To revert swap the flag to --no-assume-unchanged
-String WIFI_SSIDs[] = {"brum-hl", "brum-sp"};
-String WIFI_PASSWORD = "brum-pass-1234";
+String WIFI_SSIDs[] = {"ssid_1", "ssid_2"};
+String WIFI_PASSWORD = "password_for_ssid_1_and_ssid_2";
 
 #endif
