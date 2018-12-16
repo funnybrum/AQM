7d5f19edde41116f660d01bbdeefc40117ec4529
funnybrum <funnybrum@gmail.com>
2018-12-16 09:20:07 +0200

    Finishing the settings extraction.

--- lib/common/Settings.h
+++ lib/common/Settings.h
@@ -2,36 +2,7 @@
 #define _SETTINGS_H_
 
 #include "Logger.h"
-
-// struct SettingsData {
-//     uint8_t checksum;
-//     struct Network {
-//         char hostname[64];
-//         char ssid[32];
-//         char password[32];
-//     } network;
-//     struct AQSensor {
-//         uint8_t sensorCalibration[BSEC_MAX_STATE_BLOB_SIZE];
-//         // Actual correction = value / 10. I.e. value of -23 is -2.3C. Same apply for humidity.
-//         int16_t temperatureOffset;
-//         int16_t humidityOffset;
-//         int16_t badAQResistance;
-//         int16_t goodAQResistance;
-//         uint16_t calibrationPeriod;
-//     } aqSensor;
-
-//     struct LED {
-//         uint16_t blinkInterval;
-//     } led;
-
-//     struct InfluxDB {
-//         bool enable;
-//         char address[64];
-//         char database[16];
-//         uint16_t pushInterval;
-//         uint16_t collectInterval;
-//     } influxDB;
-// };
+#include "Arduino.h"
 
 extern Logger logger;
 
@@ -67,13 +38,16 @@ class Settings {
         void loop() {
         }
 
-        void save();
+        void save() {
+            logger.log("Writing state to EEPROM.");
+            writeToEEPROM();
+        }
 
         void erase() {
             logger.log("Erasing EEPROM.");
             memset(this->settingsData, 0, settingsSize);
             initFunction();
-            this->writeToEEPROM();
+            writeToEEPROM();
         }
 
     private:
@@ -88,7 +62,7 @@ class Settings {
 
         Logger logger;
         void* settingsData;
-        int16_t settingsSize;
+        uint16_t settingsSize;
         void (*initFunction)(void);
 };
 
